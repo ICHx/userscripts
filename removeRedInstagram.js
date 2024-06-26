083cd4a464715c5a73e53ef6a8d500cace745170
ric-mb <null>
2024-06-26 17:09:33 +0800

    add instagram script(rename)

--- removeRedInstagram.js
+++ removeRedInstagram.js
@@ -1,14 +1,11 @@
 // ==UserScript==
-// @name     Remove Threads link and red badge on instagram.com
+// @name     Remove Threads link and red badge on instagram.com(fixed)
 // @version  1.0
 // @description  Removes the div with the href https://www.threads.net/ on instagram.com without using the class xdy9tzy
-// @author    Jonathan Woolf
+// @author    Jonathan Woolf, ICHx, ChatGPT
 // @match    https://www.instagram.com/*
 // @grant    none
 // @license  Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
-// @namespace https://greasyfork.org/users/1008047
-// @downloadURL https://update.greasyfork.org/scripts/478959/Remove%20Threads%20link%20and%20red%20badge%20on%20instagramcom.user.js
-// @updateURL https://update.greasyfork.org/scripts/478959/Remove%20Threads%20link%20and%20red%20badge%20on%20instagramcom.meta.js
 // ==/UserScript==
 
 // Define a function to remove red elements
